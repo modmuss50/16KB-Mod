87710a5aac9def1b6979fb82abfa2b88145b1dc2
modmuss50 <m123world@gmail.com>
2015-03-06 13:32:31 +0000

    Added a basic gui, need to compile this at some point :P

--- src/modmuss50/m.java
+++ src/modmuss50/m.java
@@ -2,58 +2,94 @@ package modmuss50;
 
 import net.minecraft.block.material.MapColor;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiOptions;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockPos;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.FMLClientHandler;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.List;
 
 @Mod(modid = "16k", name = "16k", version = "0.16")
 public class m {
 
 	@Mod.EventHandler
-	private void i(FMLInitializationEvent event) {
+	private void i(FMLInitializationEvent e) {
 		MinecraftForge.EVENT_BUS.register(this);
+		FMLCommonHandler.instance().bus().register(this);
+	}
+
+	int lastx = 0;
+	int lastz = 0;
+
+	@SubscribeEvent
+	public void r(RenderGameOverlayEvent e) {
+		final EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+		if (lastx != (int) player.posX || lastz != (int) player.posZ) {
+			computeMap(64, 64, player);
+			lastx = (int) player.posX;
+			lastz = (int) player.posZ;
+		}
+		mapTexture.drawMap(10, 10, 300F);
+	}
+
+	@SubscribeEvent()
+	public void onGuiInit(GuiScreenEvent.InitGuiEvent.Post evt) {
+		if (evt.gui instanceof GuiOptions) {
+			List<GuiButton> buttonList = evt.buttonList;
+			GuiButton button = new GuiButton(405, 1, 1, 45, 20, "16k map");
+			buttonList.add(button);
+		}
 	}
 
 	@SubscribeEvent
-	public void r(RenderGameOverlayEvent event){
-		final EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-		computeMap(64, 64, player);
-		mapTexture.drawMap(10, 10, 300F);
+	public void onActionPerformed(GuiScreenEvent.ActionPerformedEvent evt) {
+		if (evt.gui instanceof GuiOptions) {
+			if (evt.button.id == 405) {
+				FMLClientHandler.instance().showGuiScreen(new gui(evt.gui));
+			}
+		}
 	}
 
 	public ct mapTexture = new ct(64, 64);
 
 	private void computeMap(int width, int height, EntityPlayer player) {
 		int zoom = 1;
+		int startHight = 256;
 		final int[] textureData = new int[width * height];
 		for (int i = 0; i < width; ++i) {
 			for (int j = 0; j < height; ++j) {
-				int x = i + (int) player.posX - height /2 * zoom;
+				int x = i + (int) player.posX - height / 2 * zoom;
 				int z = j + (int) player.posZ - width / 2 * zoom;
 				int y;
-				int startHight = 256;
 				for (y = startHight; y >= 0; y = y - 1) {
 					if (player.worldObj.getBlockState(new BlockPos(x, y, z)).getBlock() != Blocks.air) {
+						startHight = y + 30;
 						break;
 					}
 				}
 				int color = Minecraft.getMinecraft().thePlayer.worldObj.getBlockState(new BlockPos(x, y, z)).getBlock().getMapColor(Minecraft.getMinecraft().thePlayer.worldObj.getBlockState(new BlockPos(x, y, z))).colorIndex;
 				textureData[i + j] = color;
-				mapTexture.setColor(i , j , MapColor.mapColorArray[color].colorValue);
+				mapTexture.setColor(i, j, MapColor.mapColorArray[color].colorValue);
 			}
 		}
-		mapTexture.setColor( height /2 , width /2 , MapColor.redColor.colorValue);
-		mapTexture.setColor( height /2 + 1, width /2 , MapColor.redColor.colorValue);
-		mapTexture.setColor( height /2 , width /2 + 1, MapColor.redColor.colorValue);
-		mapTexture.setColor( height /2  + 1, width /2  + 1, MapColor.redColor.colorValue);
+		mapTexture.setColor(height / 2, width / 2, MapColor.redColor.colorValue);
+		mapTexture.setColor(height / 2 + 1, width / 2, MapColor.redColor.colorValue);
+		mapTexture.setColor(height / 2, width / 2 + 1, MapColor.redColor.colorValue);
+		mapTexture.setColor(height / 2 + 1, width / 2 + 1, MapColor.redColor.colorValue);
 	}
 }
 
@@ -92,5 +128,33 @@ class ct {
 		worldRenderer.addVertexWithUV(screenX + 0, screenY + 0, zLevel, (clipX + 0) * f, (clipY + 0) * f1);
 		tessellator.draw();
 	}
+}
+
+class gui extends GuiScreen {
+	private GuiScreen parent;
+
+	public gui(GuiScreen parent) {
+		this.parent = parent;
+	}
+
+	@Override
+	public void initGui() {
+		this.buttonList.add(new GuiButton(1, this.width / 2 - 75, this.height - 38, I18n.format("gui.done")));
+	}
+
+	@Override
+	public void actionPerformed(GuiButton button) {
+		if (button.enabled && button.id == 1) {
+			FMLClientHandler.instance().showGuiScreen(parent);
+		}
+	}
+
+	@Override
+	public void drawScreen(int x, int y, float f) {
+		this.drawDefaultBackground();
+		this.drawCenteredString(this.fontRendererObj, "16k map settings", this.width / 2, 40, 0xFFFFFF);
+		super.drawScreen(x, y, f);
+	}
+
 }
 
